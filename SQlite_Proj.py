f92abfe42d55c71399460d8a73a1ce12356e4f75
Jack <34298422+jackRL1@users.noreply.github.com>
2019-03-20 12:39:50 -0700

    Add files via upload

---
+++ SQlite_Proj.py
@@ -0,0 +1,47 @@
+import sqlite3
+import time
+import datetime
+import random
+
+# creates a connection and database file to store tables. 
+conn = sqlite3.connect('example.db')
+
+# cursor object. 
+c = conn.cursor()
+
+#method for creating a table. 
+def creat_table():
+    c.execute('CREATE TABLE IF NOT EXISTS newTable(unix REAL, datestamp TEXT, keyword TEXT, value REAL)')
+
+#simple data entry method
+def data_entry():
+    c.execute("INSERT INTO newTable VALUES(213, '2019-03-15', 'Python', 45)")
+    conn.commit()
+    c.close()
+    conn.close()
+
+#dynamic data entry for adding data and time stamp, code used, and random generated value.
+def dynamic_data_entry():
+    unix = time.time()
+    date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+    keyword = 'Python'
+    value = random.randrange(0,100)
+    #list of tuples that are being added to the database
+    adding = [(unix, date, keyword, value)]
+    #inserts the values from adding into the data table. 
+    c.executemany('INSERT INTO newtable VALUES (?, ?, ?, ?)', adding)
+    #commits changes to the table
+    conn.commit()
+
+
+# Function call for the creating a table called newTable
+creat_table()
+
+# For loop to generate data inserted into the newTable
+for i in range(10):
+    dynamic_data_entry()
+    time.sleep(0.5)
+
+#closes the connection and cursor. 
+c.close()
+conn.close()
